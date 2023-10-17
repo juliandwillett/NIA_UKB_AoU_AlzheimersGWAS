0f9a8ba1643c8755bb55e195fe844037dc7b49d7
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-10-17 13:13:59 -0400

    Update RunMETAL.sh

--- RunMETAL.sh
+++ RunMETAL.sh
@@ -4,10 +4,11 @@ awk 'NR==1 { $13 = "Pval"}1' ukb_proxy_all_anc_common_with_pval.txt > ukb_proxy_
 awk 'NR>1 {$3 = $1 ":" $2 ":" $4 "," $5}1' aou_AD_any_anc_all_common_variant_gwas_tsv_with_pvals.txt > aou_AD_any_anc_all_common_variant_gwas_tsv_with_pvals_ids.txt
 awk 'NR>1 {$3 = $1 ":" $2 ":" $4 "," $5}1' ukb_proxy_all_anc_common_with_pvals.txt > ukb_proxy_all_anc_common_with_pvals_ids.txt
 
+# Run METAL
 /n/home13/dprokopenko/bin/metal
+SCHEME STDERR
 
 MARKER   ID
-WEIGHT   N
 ALLELE   ALLELE1 ALLELE0
 FREQ     A1FREQ
 EFFECT   BETA
@@ -15,10 +16,9 @@ STDERR   SE
 PVAL     Pval
 PROCESS /n/home09/jwillett/true_lab_storage/00_AoU/GWAS_Data/aou_AD_any_anc_all_common_variant_gwas_tsv_with_pvals_ids.txt
 
-MARKER   SNP
-ALLELE   EFFECT_ALLELE NON_EFFECT_ALLELE
-FREQ     FREQ_EFFECT
-WEIGHT   N
+MARKER   ID
+ALLELE   ALLELE1 ALLELE0
+FREQ     A1FREQ
 EFFECT   BETA
 STDERR   SE
 PVAL     Pval
