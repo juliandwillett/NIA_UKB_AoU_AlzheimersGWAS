cbca04f8a6c484fb06a75a8de702d4aa02bfe3c4
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-10-19 09:50:26 -0400

    Update Regenie_Step2_AllAnc_AllVariants.sh

--- Regenie_Step2_AllAnc_AllVariants.sh
+++ Regenie_Step2_AllAnc_AllVariants.sh
@@ -50,8 +50,8 @@ for ((i=1; i<=16; i++)); do \
         --out aou_step2_rg_${curr_chr}_allvar_anc_all \
         --minMAC 20
   done
-  gsutil -o GSUtil:parallel_composite_upload_threshold=104857600 -m cp -r -n aou_step2_rg_${curr_chr}_allvar_anc_* gs://fc-secure-4029af59-df13-4d1b-b22c-2ae64cb3dc67/data/rg_results_all_anc_all_var_mac_20/
-  gsutil -o GSUtil:parallel_composite_upload_threshold=104857600 -m cp -r -n *.log gs://fc-secure-4029af59-df13-4d1b-b22c-2ae64cb3dc67/data/rg_results_all_anc_all_var_mac_20/
+  gsutil -o GSUtil:parallel_composite_upload_threshold=104857600 -m cp -r -n aou_step2_rg_${curr_chr}_allvar_anc_* gs://fc-secure-4029af59-df13-4d1b-b22c-2ae64cb3dc67/data/rg_results_all_anc_all_var_mac_20/ ;\
+  gsutil -o GSUtil:parallel_composite_upload_threshold=104857600 -m cp -r -n *.log gs://fc-secure-4029af59-df13-4d1b-b22c-2ae64cb3dc67/data/rg_results_all_anc_all_var_mac_20/ ;\
   rm plink_${curr_chr}_multi_split_merged* ;\
 done
 
