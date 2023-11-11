7425c72d84013f4df53845f00dd0e335f70a0c37
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-11-10 19:01:30 -0500

    Update GetEnrichedTissues.R

--- GetEnrichedTissues.R
+++ GetEnrichedTissues.R
@@ -1,7 +1,7 @@
 library(glue)
 
-x = read_csv("GTEx Portal.csv")
+x = read_csv("GTEx Portal.csv") # plug in Gene to GTEx, and download the CSV file
 s = ""
 for (t in unique((x %>% filter(`P-Value` <= 1e-5))$Tissue)) {
-    s = paste(s,glue("{t} (p = {min((x %>% filter(Tissue == t))$`P-Value`)})"))
+    s = paste(s,glue("{t} (p = {min((x %>% filter(Tissue == t))$`P-Value`)}),"))
 }
