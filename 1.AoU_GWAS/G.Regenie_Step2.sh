2191fcdca894600d1fed69aa2579ebe8858d77c5
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-11-15 16:35:37 -0500

    Update G.Regenie_Step2.sh

--- 1.AoU_GWAS/G.Regenie_Step2.sh
+++ 1.AoU_GWAS/G.Regenie_Step2.sh
@@ -1,5 +1,21 @@
+# Get necessary files
+wget https://s3.amazonaws.com/plink2-assets/alpha3/plink2_linux_avx2_20221024.zip
+unzip plink2_linux*
+wget https://github.com/rgcgithub/regenie/releases/download/v3.2.8/regenie_v3.2.8.gz_x86_64_Linux.zip
+unzip regenie_v3.2.8.gz_x86_64_Linux.zip
+
 # Get files for current chromosome
+curr_chr="chr19"
 gsutil -m cp -rn $WORKSPACE_BUCKET/data/pgen_minimal_qc/plink_chr19_* .
+
+# Do QC, if it has not been run already
+./plink2 --pfile plink_${curr_chr}_multi_split \
+        --geno 0.1 --mind 0.1 --hwe 1e-15 \
+        --make-pgen --out plink_${curr_chr}_allvar_anc_all
+
+# revise psam file given the empty column being dropped
+awk 'NR==1 {print "FID\tIID"} NR>1 {print "0\t" $1}' plink_chr19_allvar_anc_all.psam > tmp
+mv tmp plink_${curr_chr}_allvar_anc_all.psam
 
 # Get files for pheno/covar/step1
 gsutil -m cp -rn $WORKSPACE_BUCKET/data/regenie_* .
@@ -8,15 +24,16 @@ gsutil -m cp -rn $WORKSPACE_BUCKET/data/regenie/* .
 # Run regenie. I recommend the "--mcc" parameter for additional QC
 ./regenie_v3.2.8.gz_x86_64_Linux \
     --step 2 \
-    --pfile plink_${curr_chr}_multi_split \
-    --phenoFile regenie_pheno_zeroFID.txt \
-    --covarFile regenie_covars_20pcs_withsex_zeroFID.txt \
-    --bt --mcc \
+    --pgen plink_${curr_chr}_allvar_anc_all \
+    --phenoFile regenie_pheno_revised.txt \
+    --covarFile regenie_covar_revised.txt \
+    --bt --mcc --firth-se \
     --firth --approx --pThresh 0.01 \
     --pred revised_20pcs_pred_sexcovar.list \
     --bsize 400 \
     --out aou_step2_rg_${curr_chr}_firthallvariants \
-    --minMAC 20
+    --minMAC 20 \
+    --phenoCol AD_any
 
 # Backup results
 gsutil -m cp -rn aou_step2_rg_${curr_chr}_firthallvariants $WORKSPACE_BUCKET/data/rg_results/
