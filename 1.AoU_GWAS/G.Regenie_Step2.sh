9ed1845d6e22d45e875d9c926fc99fb2809a42a9
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-11-15 18:49:28 -0500

    Update G.Regenie_Step2.sh

--- 1.AoU_GWAS/G.Regenie_Step2.sh
+++ 1.AoU_GWAS/G.Regenie_Step2.sh
@@ -5,8 +5,9 @@ wget https://github.com/rgcgithub/regenie/releases/download/v3.2.8/regenie_v3.2.
 unzip regenie_v3.2.8.gz_x86_64_Linux.zip
 
 # Get files for current chromosome
+bucket="gs://fc-secure-4029af59-df13-4d1b-b22c-2ae64cb3dc67"
 curr_chr="chr19"
-gsutil -m cp -rn $WORKSPACE_BUCKET/data/pgen_minimal_qc/plink_${curr_chr}_* .
+gsutil -m cp -rn $bucket/data/pgen_minimal_qc/plink_${curr_chr}_* .
 
 # Do QC, if it has not been run already
 ./plink2 --pfile plink_${curr_chr}_multi_split \
@@ -14,12 +15,15 @@ gsutil -m cp -rn $WORKSPACE_BUCKET/data/pgen_minimal_qc/plink_${curr_chr}_* .
         --make-pgen --out plink_${curr_chr}_allvar_anc_all
 
 # revise psam file given the empty column being dropped
-awk 'NR==1 {print "#FID\tIID\tSEX"} NR>1 {print "0\t" $1 "\t" $2}' plink_chr19_allvar_anc_all.psam > tmp
+awk 'NR==1 {print "#FID\tIID\tSEX"} NR>1 {print "0\t" $1 "\t" $2}' plink_${curr_chr}_allvar_anc_all.psam > tmp
 mv tmp plink_${curr_chr}_allvar_anc_all.psam
 
 # Get files for pheno/covar/step1
-gsutil -m cp -rn $WORKSPACE_BUCKET/data/regenie_* .
-gsutil -m cp -rn $WORKSPACE_BUCKET/data/regenie/* .
+gsutil -m cp -rn $bucket/data/regenie_* .
+gsutil -m cp -rn $bucket/data/regenie/* .
+
+# Rename workspace in pred file, to enable running on parallel workspaces
+awk '{gsub("duplicateofalzheimersgwastake5", "duplicateofalzheimersgwastake5d1", $2)} 1' aou_step1_rg_array_anc_all_pred.list > revised_pred.list
 
 # Run regenie. I recommend the "--mcc" parameter for additional QC
 ./regenie_v3.2.8.gz_x86_64_Linux \
@@ -29,11 +33,11 @@ gsutil -m cp -rn $WORKSPACE_BUCKET/data/regenie/* .
     --covarFile regenie_covar_revised.txt \
     --bt --mcc --firth-se \
     --firth --approx --pThresh 0.01 \
-    --pred aou_step1_rg_array_anc_all_pred.list \
+    --pred revised_pred.list \
     --bsize 400 \
     --out aou_step2_rg_${curr_chr}_firthallvariants \
     --minMAC 20 \
     --phenoCol AD_any
 
 # Backup results
-gsutil -m cp -rn aou_step2_rg_${curr_chr}_firthallvariants $WORKSPACE_BUCKET/data/rg_results_mcc/
+gsutil -m cp -rn aou_step2_rg_${curr_chr}_firthallvariants $bucket/data/rg_results_mcc/
