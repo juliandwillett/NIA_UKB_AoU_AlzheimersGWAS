da4ec06caebe93af0610f21720d22eb1db3fd58a
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-11-18 16:14:48 -0500

    Update A.METAL_BASH.sh

--- 3.MetaAnalysis/A.METAL_BASH.sh
+++ 3.MetaAnalysis/A.METAL_BASH.sh
@@ -1,5 +1,7 @@
 # first ensure the ID columns are the same format in GWAS files, and create a P value column
 awk -v OFS='\t' 'NR==1 {print $0 "\tPval"} NR>1 {$15 = 10^(-1 * $12); print }' aou_AD_any_anc_all_gwas.txt > aou_AD_any_anc_all_gwas_pvals.txt
+awk -v OFS='\t' 'NR>1 {$3 = $1 ":" $2 ":" $4 "," $5}1' aou_AD_any_anc_all_gwas_pvals.txt > aou_AD_any_anc_all_gwas_pvals_ids.txt #necessary to make meta analysis work
+
 awk -v OFS='\t' '{$NF = 10^(-1 * $12); print }' ukb_proxy_all_anc.txt > ukb_proxy_all_anc_with_pval.txt
 awk -v OFS='\t' 'NR==1 { $13 = "Pval"}1' ukb_proxy_all_anc_with_pval.txt > ukb_proxy_all_anc_with_pvals.txt
 awk -v OFS='\t' 'NR>1 {$3 = $1 ":" $2 ":" $4 "," $5}1' ukb_proxy_all_anc_with_pvals.txt > ukb_proxy_all_anc_with_pvals_ids.txt
