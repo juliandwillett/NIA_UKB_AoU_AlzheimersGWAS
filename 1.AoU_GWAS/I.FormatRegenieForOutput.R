4ce44ffeedfda8954a5b2a25bbff3db4d6d9309e
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-11-18 10:26:01 -0500

    Update I.FormatRegenieForOutput.R

--- 1.AoU_GWAS/I.FormatRegenieForOutput.R
+++ 1.AoU_GWAS/I.FormatRegenieForOutput.R
@@ -2,26 +2,29 @@
 # I dealt with this by making subsets of each chromosome to be under 100 Mb, which worked better
 # You also have to slightly adjust the code to ensure that more than 20 people for each hit, per AoU privacy requirements
 
-# Read in data
-outcome = "AD_any"
-chr = "chr5"
-data = vroom(glue("aou_step2_rg_{chr}_firthallvariants_{outcome}.regenie"))
+for (c in 1:22) {
+    # Read in data
+    print(glue("Curr chr: {c}"))
+    outcome = "AD_any"
+    chr = glue("chr{c}")
+    data = vroom(glue("aou_step2_rg_{chr}_firthallvariants_{outcome}.regenie"))
 
-# Ensure exported data meets privacy requirements
-data_min20 = data %>% filter(A1FREQ * N >= 20)
-min(data_min20$A1FREQ * data_min20$N)
+    # Ensure exported data meets privacy requirements
+    data_min20 = data %>% filter(A1FREQ * N >= 20)
+    min(data_min20$A1FREQ * data_min20$N)
 
-# break into pieces
-data_min20_categories = data_min20 %>% mutate(Category = ifelse(row_number() < 1e6,"A",
+    # break into pieces
+    data_min20_categories = data_min20 %>% mutate(Category = ifelse(row_number() < 1e6,"A",
                                                                ifelse(row_number() < 2e6,"B",
                                                                       ifelse(row_number()<3e6,"C",
                                                                             ifelse(row_number()<4e6,"D",
                                                                                   ifelse(row_number()<5e6,"E","F"))))))
-subsets = split(data_min20_categories, data_min20_categories$Category)
-
-# write each piece
-for (category in names(subsets)) {
-  subset_data <- subsets[[category]]
-  filename <- glue("aou_step2_rg_{chr}_allvar_anc_all_{outcome}_min20N_{category}.regenie")  # Define the file name
-  vroom_write(subset_data, filename)
+    subsets = split(data_min20_categories, data_min20_categories$Category)
+    
+    # write each piece
+    for (category in names(subsets)) {
+          subset_data <- subsets[[category]]
+          filename <- glue("aou_step2_rg_{chr}_allvar_anc_all_{outcome}_min20N_{category}.regenie")  # Define the file name
+          vroom_write(subset_data, filename)
+    }
 }
