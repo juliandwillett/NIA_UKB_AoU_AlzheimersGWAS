5083eebb553b23ff5a385cc425794d7744db27d8
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-12-02 18:11:16 -0500

    Update A.FlipAllelesToMinor.sh

--- 3.MetaAnalysis/A.FlipAllelesToMinor.sh
+++ 3.MetaAnalysis/A.FlipAllelesToMinor.sh
@@ -1,3 +1,5 @@
+# While METAL is supposed to be able to handle different reference alleles, I was less confident of this (select variants were not captured)
+
 awk 'BEGIN {OFS="\t"} NR==1 {print; next} { if ($6 > 0.5) { temp=$4; $4=$5; $5=temp; $9=-$9; $6=1-$6 } print }' \
 /n/home09/jwillett/true_lab_storage/Data_Links/AoU_GWAS/NON_MCC_GWAS/aou_AD_any_anc_all_gwas_pvals_ids_chrompos_firthse.txt > \
 /n/home09/jwillett/true_lab_storage/Data_Links/AoU_GWAS/NON_MCC_GWAS/aou_AD_any_anc_all_gwas_pvals_ids_chrompos_firthse_allminoralleles.txt
