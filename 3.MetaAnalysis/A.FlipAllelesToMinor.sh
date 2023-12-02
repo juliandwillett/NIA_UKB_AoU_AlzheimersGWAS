0364b14b0e34655aebc4e880ddc6e0302fc40223
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-12-02 18:10:40 -0500

    Create A.FlipAllelesToMinor.sh

---
+++ 3.MetaAnalysis/A.FlipAllelesToMinor.sh
@@ -0,0 +1,7 @@
+awk 'BEGIN {OFS="\t"} NR==1 {print; next} { if ($6 > 0.5) { temp=$4; $4=$5; $5=temp; $9=-$9; $6=1-$6 } print }' \
+/n/home09/jwillett/true_lab_storage/Data_Links/AoU_GWAS/NON_MCC_GWAS/aou_AD_any_anc_all_gwas_pvals_ids_chrompos_firthse.txt > \
+/n/home09/jwillett/true_lab_storage/Data_Links/AoU_GWAS/NON_MCC_GWAS/aou_AD_any_anc_all_gwas_pvals_ids_chrompos_firthse_allminoralleles.txt
+
+awk 'BEGIN {OFS="\t"} NR==1 {print; next} { if ($6 > 0.5) { temp=$4; $4=$5; $5=temp; $9=-$9; $6=1-$6 } print }' \
+/n/home09/jwillett/true_lab_storage/Data_Links/UKB_GWAS_Data/proxy_files_Step1_2_corrected_tab_withids.txt > \
+/n/home09/jwillett/true_lab_storage/Data_Links/UKB_GWAS_Data/proxy_files_Step1_2_corrected_tab_withids_allminoralleles.txt
