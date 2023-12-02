50be447ab71882cf499e0c1e5b6a4e38b9a30b5e
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-12-02 12:14:17 -0500

    Update A.METAL_BASH.sh

--- 3.MetaAnalysis/A.METAL_BASH.sh
+++ 3.MetaAnalysis/A.METAL_BASH.sh
@@ -13,8 +13,8 @@ metal METAL_script.txt
 # functions to clean up data for further processing and expedite analysis (intersections with single GWAS, for example)
 mv METAANALYSIS1.TBL aou_ukb_allvar_meta_analysis.TBL
 
-# Add CHR and POS columns, to make sorting easier
-awk 'BEGIN{FS=" "; OFS="\t"} NR==1 {print $0 "\tCHR\tPOS"} NR>1 {split($1, values, ":"); $16 = values[1]; $17 = values[2]; print $0}' \
+# Add CHR, POS, and FAVOR_ID columns, to make sorting easier and enable later intersections (ie intersection with Bellenguez et al for comparison)
+awk 'BEGIN{FS=" "; OFS="\t"} NR==1 {print $0 "\tCHR\tPOS\tID"} NR>1 {split($1, values, ":"); $16 = values[1]; $17 = values[2]; $18 = $16 "-" $17 "-" $2 "-" $3; print $0}' \
   aou_ukb_allvar_meta_analysis.TBL > aou_ukb_allvar_meta_analysis_IDcolon_chrposrefalt_cols.TBL
 
 # Isolate GW significant hits to focus the analysis (and make R code work more efficiently)
