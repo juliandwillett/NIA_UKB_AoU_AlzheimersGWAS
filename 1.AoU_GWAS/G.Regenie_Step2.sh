fb318cc62f7232b77459cf425d093dd08391fbe0
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-12-22 09:33:41 -0500

    Update G.Regenie_Step2.sh

--- 1.AoU_GWAS/G.Regenie_Step2.sh
+++ 1.AoU_GWAS/G.Regenie_Step2.sh
@@ -49,6 +49,7 @@ ancestries=(eur afr amr)
 for ((chr=1;chr<=22;chr++)); do \
         curr_chr="chr${chr}" ;\
         for anc in "${ancestries[@]}"; do \
+                awk '{gsub("duplicateofalzheimersgwastake5d1", "duplicateofalzheimersgwastake5", $2)} 1' aou_step1_rg_array_anc_${anc}_pred.list > revised_pred.list
                 ./regenie_v3.2.8.gz_x86_64_Linux \
                     --step 2 \
                     --pgen plink_${curr_chr}_allvar_anc_all \
@@ -56,7 +57,7 @@ for ((chr=1;chr<=22;chr++)); do \
                     --covarFile regenie_covar.txt \
                     --bt --firth-se \
                     --firth --approx --pThresh 0.01 \
-                    --pred aou_step1_rg_array_anc_${anc}_pred.list \
+                    --pred revised_pred.list \
                     --bsize 400 \
                     --out aou_step2_rg_${curr_chr}_firthallvariants_${anc} \
                     --minMAC 20 \
