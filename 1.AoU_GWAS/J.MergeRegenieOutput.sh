d44769634a17637900107b5b36eabf251b9b14c3
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2023-12-29 17:57:18 -0500

    Update J.MergeRegenieOutput.sh

--- 1.AoU_GWAS/J.MergeRegenieOutput.sh
+++ 1.AoU_GWAS/J.MergeRegenieOutput.sh
@@ -4,3 +4,13 @@ head -n 1 aou_step2_rg_chr6_allvar_anc_all_AD_any_min20N_C.regenie > aou_AD_any_
 for file in *.regenie; do \
     tail -n +2 "$file" >> aou_AD_any_anc_all_gwas.txt ;\
 done
+
+##
+# Do for ancestry stratified datasets
+ancestries=(eur afr amr)
+for anc in "${ancestries[@]}"; do
+    head -n 1 aou_step2_rg_chr7_allvar_anc_${anc}_AD_any_min20N_A.regenie > aou_AD_any_anc_${anc}_gwas.txt
+    for file in *.regenie; do \
+        tail -n +2 "$file" >> aou_AD_any_anc_all_gwas.txt ;\
+    done
+done
