473b47cd14c11b403a71be5e54400103f00db689
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-01-03 11:41:56 -0500

    Create C.Remove_HWE_Variants.bash

---
+++ 2.UKB_GWAS/C.Remove_HWE_Variants.bash
@@ -0,0 +1,5 @@
+### HWE vals calculated separately, so need to filter the GWAS results for those with a pval <= 1e-15
+# First, isolate lines of interest (those with a p-val greater than the value)
+awk '$8 > 1e-15 {print $2}' /n/holystore01/LABS/tanzi_lab/Users/Mo/UKB/Interaction/sorted_hardy
+
+awk '$8 <= 1e-15 {print}' sorted_hardy | wc -l
