ef670f01f267b4fcb5be853121cba17ec42aca8e
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-01-04 11:09:01 -0500

    Update C.Remove_HWE_Variants.bash

--- 2.UKB_GWAS/C.Remove_HWE_Variants.bash
+++ 2.UKB_GWAS/C.Remove_HWE_Variants.bash
@@ -2,4 +2,4 @@
 # First, isolate lines of interest (those with a p-val greater than the value)
 awk '$8 > 1e-15 {print $2}' /n/holystore01/LABS/tanzi_lab/Users/Mo/UKB/Interaction/sorted_hardy
 
-awk '$8 <= 1e-15 {print}' sorted_hardy | wc -l
+awk '$10 <= 1e-15 {print}' /n/holystore01/LABS/tanzi_lab/Users/Mo/UKB/Interaction/sorted_hardy | wc -l
