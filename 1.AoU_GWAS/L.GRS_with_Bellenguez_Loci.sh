aa27b1ba7af1a63c8d42a846a46eaa573a37c5c1
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-01-12 14:59:32 -0500

    Update L.GRS_with_Bellenguez_Loci.sh

--- 1.AoU_GWAS/L.GRS_with_Bellenguez_Loci.sh
+++ 1.AoU_GWAS/L.GRS_with_Bellenguez_Loci.sh
@@ -12,8 +12,8 @@ grs_ids = data.frame(ID
   
 vroom_write(plink_pos_filter,"grs_hits_plink_pos.bed")
 vroom_write(grs_df,"grs_df_for_score.txt")
-vroom_write(
 
+###########
 # Start filtering/processing the AoU plink files for these hits. Do on AoU
 if ((curr_chr>=17)) ; then
     ./plink2 --pfile plink_chr${curr_chr}_multi_split \
@@ -27,22 +27,9 @@ else
         --extract bed1 grs_hits_plink_pos.bed
 fi
 
-# Next run the GRS. Following: https://choishingwan.github.io/PRS-Tutorial/plink/
-awk 'NR>1 {print $1}' grs_df_for_score.txt > study_snps.txt ;\
-awk '{print $1,$4}' grs_df_for_score.txt > SNP.pvalue ;\
-echo "0.001 0 0.001" > range_list;\
-echo "0.05 0 0.05" >> range_list ;\
-echo "0.1 0 0.1" >> range_list ;\
-echo "0.2 0 0.2" >> range_list ;\
-echo "0.3 0 0.3" >> range_list ;\
-echo "0.4 0 0.4" >> range_list ;\
-echo "0.5 0 0.5" >> range_list ;
-
-./plink2 \
-    --pfile tmp \
-    --score grs_df_for_score.txt 1 2 3 header \
-    --q-score-range range_list SNP.pvalue \
-    --extract study_snps.txt \
-    --out grs_chr${curr_chr}
-
-# Plotting ideas: https://github.com/benjacobs123456/PRS_UKB_MRI
+# Then get the Hardy values to show what is going on with many of the hits
+curr_chr="1"
+./plink2 --pfile plink_chr${curr_chr}_multi_split_merged \
+--set-all-var-ids @:#:\$r,\$a --new-id-max-allele-len 10000 \
+--missing --hardy --out grs_files/${curr_chr} \
+--extract bed1 grs_hits_plink_pos_sorted.bed
