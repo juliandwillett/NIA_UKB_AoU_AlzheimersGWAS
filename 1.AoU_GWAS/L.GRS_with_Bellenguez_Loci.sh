84c1c061f06a9711000a50c9b423be3b6312b88f
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-01-12 15:47:48 -0500

    Update L.GRS_with_Bellenguez_Loci.sh

--- 1.AoU_GWAS/L.GRS_with_Bellenguez_Loci.sh
+++ 1.AoU_GWAS/L.GRS_with_Bellenguez_Loci.sh
@@ -42,7 +42,6 @@ for ((chr=17;chr<=22;chr++)) ; do \
     --missing --hardy --out grs_files/${curr_chr} \
     --extract bed1 grs_hits_plink_pos_sorted.bed ;\
 done
-
 # merge the results for export
 head -n 1 grs_files/chr4.hardy > grs_files/hardy_values.txt ;\
 for file in grs_files/*.hardy; do \
@@ -53,7 +52,32 @@ done
 # Given that some of the variants are present in fewer than 20 individuals, do the data processing in AoU workbench via R
 alleles = vroom("grs_df_for_score.txt")
 df = vroom("grs_files/hardy_values.txt") %>% arrange(`#CHROM`,ID) %>% 
-    separate(ID,into = c("CHR", "POS"), sep = ":") %>%
-    mutate(IDrev = glue("{CHR}:{POS}:{AX},{A1}")) %>% filter(ID %in% alleles$ID | IDrev %in% alleles$ID)
+    separate(ID,into = c("CHR", "POS"), sep = ":",remove = F) %>%
+    mutate(IDrev = glue("{CHR}:{POS}:{AX},{A1}")) %>% filter(ID %in% alleles$ID | IDrev %in% alleles$ID) %>%
+    distinct()
 print(glue("Orig len: {nrow(alleles)}. Remaining len: {nrow(df)}"))
 head(df)
+nrow(df %>% filter(P <= 1e-15))
+nrow(alleles)
+
+#############
+# Those are the stats pre-QC, so what about after QC, aside from HWE?
+for ((chr=1;chr<17;chr++)) ; do \
+  curr_chr="chr${chr}" ;\
+  ./plink2 --pfile plink_${curr_chr}_multi_split_merged --geno 0.1 \
+    --set-all-var-ids @:#:\$r,\$a --new-id-max-allele-len 10000 \
+    --missing --hardy --out grs_files_postqc_except_hwe/${curr_chr} \
+    --extract bed1 grs_hits_plink_pos_sorted.bed ;\
+done
+for ((chr=17;chr<=22;chr++)) ; do \
+  curr_chr="chr${chr}" ;\
+  ./plink2 --pfile plink_${curr_chr}_multi_split --geno 0.1 \
+    --set-all-var-ids @:#:\$r,\$a --new-id-max-allele-len 10000 \
+    --missing --hardy --out grs_files_postqc_except_hwe/${curr_chr} \
+    --extract bed1 grs_hits_plink_pos_sorted.bed ;\
+done
+# merge the results for export
+head -n 1 grs_files_postqc_except_hwe/chr4.hardy > grs_files_postqc_except_hwe/hardy_values.txt ;\
+for file in grs_files_postqc_except_hwe/*.hardy; do \
+    tail -n +2 "$file" >> grs_files_postqc_except_hwe/hardy_values.txt ;\
+done
