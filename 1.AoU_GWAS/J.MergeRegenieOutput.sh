e53e9758218bfe5ab9e08f3342259ff1261647a5
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-01-26 21:11:09 -0500

    Update J.MergeRegenieOutput.sh

--- 1.AoU_GWAS/J.MergeRegenieOutput.sh
+++ 1.AoU_GWAS/J.MergeRegenieOutput.sh
@@ -1,8 +1,7 @@
 # I recommend doing this after exporting from AoU, given the limited file size you can successfully export after being approved for large egress.
-head -n 1 aou_step2_rg_chr6_allvar_anc_all_AD_any_min20N_C.regenie > aou_AD_any_anc_all_gwas.txt
-
+head -n 1 aou_step2_rg_chr6_allvar_anc_all_AD_any_min20N_C.regenie > aou_ad_any_anc_all_gwas_geno_1e-1_mac20_common_pcs.txt
 for file in *.regenie; do \
-    tail -n +2 "$file" >> aou_AD_any_anc_all_gwas.txt ;\
+    tail -n +2 "$file" >> aou_ad_any_anc_all_gwas_geno_1e-1_mac20_common_pcs.txt ;\
 done
 
 ##
