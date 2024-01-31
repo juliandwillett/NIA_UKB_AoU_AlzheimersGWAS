4fe8d4436db653a1e034670d951ffeef345fe3de
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-01-31 15:38:34 -0500

    Update O.Get_Independent_Loci.sh

--- 1.AoU_GWAS/O.Get_Independent_Loci.sh
+++ 1.AoU_GWAS/O.Get_Independent_Loci.sh
@@ -18,10 +18,12 @@ separate_loci = function(df) {
 # Then bring this data into AoU (in a zip file)
 unzip locus_hits.zip
 mkdir locus_hits/beds/
+mkdir locus_hits/assoc_files/
 mkdir locus_hits/plink1_files/
 cd locus_hits
 for file in *.txt ; do \
   awk '{print $5 "\t" $6 "\t" $6}' $file > beds/$file.bed ;\
+  awk -F'\t' '{print $2 "\t" $14}' $file > assoc_files/$file.plink ;\
 done
 cd ..
 
@@ -31,7 +33,11 @@ for file in locus_hits/beds/*.bed ; do \
   fname="${fname%.*}" ;\
   chr="${fname%%_*}" ;\
   ./plink2 --pfile pgen_geno_1e-1_mac_20/${chr} --extract bed1 $file \
-    --r2-phased --ld-window-r2 0 --ld-window 1000 --ld-window-kb 5000 --out locus_hits/ld_out/$fname ;\
+    --clump locus_hits/assoc_files/${fname}.txt.plink --clump-r2 0.01 --clump-id-field "ID" \
+    --clump-p-field "MetaP" --out locus_hits/clumped/$fname
+  
+  #./plink2 --pfile pgen_geno_1e-1_mac_20/${chr} --extract bed1 $file \
+  #  --r2-phased --ld-window-r2 0 --ld-window 1000 --ld-window-kb 5000 --out locus_hits/ld_out/$fname ;\
     
   # For single comparisons (to look at dprime)
   #./plink2 --pfile pgen_geno_1e-1_mac_20/chr1 --extract bed1 locus_hits/beds/chr1_1_numvar_16.txt.bed \
@@ -40,7 +46,7 @@ for file in locus_hits/beds/*.bed ; do \
 done
 
 # merge output to make comparison easier
-head -1 locus_hits/ld_out/$fname.vcor > locus_hits/ld_out/ld_comparisons.txt ;\
-for file in locus_hits/ld_out/*.vcor ; do \
-  tail -n +2 "$file" >> locus_hits/ld_out/ld_comparisons.txt ;\
+head -1 locus_hits/clumped/$fname.clumps > locus_hits/clumped/clumps.txt ;\
+for file in locus_hits/clumped/*.clumps ; do \
+  tail -n +2 "$file" >> locus_hits/clumped/clumps.txt ;\
 done
