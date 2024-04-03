9759811af9b8c95cc050e90a9df63261120f3aac
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-04-03 12:53:24 -0400

    Update K.CheckForFalsePositives_Anc.sh

--- 1.AoU_GWAS/K.CheckForFalsePositives_Anc.sh
+++ 1.AoU_GWAS/K.CheckForFalsePositives_Anc.sh
@@ -3,28 +3,39 @@
 #   removing hits that did not pass HWE in single-ancestry data.
 # So all we need are the HWE p values for single-ancestry cohorts
 
-mkdir hwe_testing ; mkdir hwe_testing/hardy_out/
-awk '{print $1 "\t" $2}' pgen_geno_1e-1_mac_20/chr1.psam > ancestries/all_ids.txt
-ancestries=(all eur afr amr sas eas mid)
-
-# First get the hits from all analyses (GWAS1, GWAS2, Meta), produce bed file. {R}
-meta_hits = vroom("/n/holystore01/LABS/tanzi_lab/Users/jwillett/00_AoU/aou_ukb_allvar_meta_analysis_IDcolon_chrposrefalt_cols_gw_sig.TBL")
-ukb_hits = vroom("/n/holystore01/LABS/tanzi_lab/Lab/UKBiobank/WGS_results/all_variants_200k_hyphen_ids_pvals_gwsig.regenie")
-aou_hits = vroom("/n/holystore01/LABS/tanzi_lab/Lab/AoU/CommonPCs_NonMCC_Geno1e-1_MAC20/aou_ad_any_anc_all_gwas_geno_1e-1_mac20_common_pcs_pvals_gwsig.txt")
-hits_ids = data.frame(ID=aou_hits$ID) %>% add_row(ID=meta_hits$MarkerName) %>% add_row(ID=ukb_hits$ID) %>% distinct()
-hits_id = hits_id %>% separate(ID,into=c("CHR","POS","A0","A1"),sep="-")
-bed_file = data.frame(CHR=hits_id_split$CHR,POSS = hits_id_split$POS,POSE = hits_id_split$POS)
-vroom_write(bed_file,"all_study_hits.bed")
+###########
+#{R} FASRC
+make_files_for_hwe = function() {
+  # get CHRPOS of key variants for HWE testing. Not using IDS here as it is slower
+  # The output also includes IDs, so I can do matching there.
+  meta_hits = vroom("aou_ukb_nia_allvar_meta_analysis_chrposrefalt_cols_gw_sig.TBL",show_col_types = F) %>%
+    mutate(CHR = as.numeric(CHR))
+  ukb_hits = vroom("../Data_Links/UKB_GWAS_Data/all_variants_200k_complete_p_id_chrpos_gwsig.regenie",show_col_types = F) %>%
+    mutate(`#CHROM` = as.numeric(`#CHROM`))
+  aou_hits = vroom("../Data_Links/AoU_GWAS/CommonPCs_NonMCC_Geno1e-1_MAC20/aou_ad_any_anc_all_gwas_geno_1e-1_mac20_common_pcs_pvals_chrpos_gwsig.txt",show_col_types = F) %>%
+    mutate(CHROM = as.numeric(CHROM))
+  nia_hits = vroom("/n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/NIAGADS_meta_chrpos_gwsig.txt",show_col_types = F) %>%
+    mutate(chr = as.numeric(chr))
+  bed_file = data.frame(CHR=meta_hits$CHR,POSS=meta_hits$POS,POSE=meta_hits$POS) %>%
+    add_row(data.frame(CHR=ukb_hits$`#CHROM`,POSS=ukb_hits$GENPOS,POSE=ukb_hits$GENPOS)) %>%
+    add_row(data.frame(CHR=aou_hits$CHROM,POSS=aou_hits$GENPOS,POSE=aou_hits$GENPOS)) %>%
+    add_row(data.frame(CHR=nia_hits$chr,POSS=nia_hits$pos,POSE=nia_hits$pos))
+  vroom_write(bed_file,"working/all_study_hits.bed")
+  print("Wrote BED file for AoU testing to: working/all_study_hits.bed")
+}
 {/R}
 
-# Prepare bed file (if testing a single GWAS, i.e. AoU)
-#awk 'NR==1 {print "CHR\tPOS\tPOS" } NR>1 {print $1 "\t" $2 "\t" $2}' aou_ad_any_anc_all_gwas_geno_1e-1_mac20_common_pcs_pvals_gwsig.txt > hwe_testing/aou_hits.bed
+###########################
+# BASH, on AoU
+mkdir hwe_testing ; mkdir hwe_testing/hardy_out/
+awk '{print $1 "\t" $2}' pgen_qc/chr1_geno_mac.psam > all_ids.txt
+ancestries=(all eur afr amr sas eas mid)
 
 # Run the hardy calculations
 for ((chr=1;chr<=22;chr++)); do \
   for anc in "${ancestries[@]}"; do \
-    ./plink2 --pfile pgen_geno_1e-1_mac_20/chr${chr} --keep ancestries/${anc}_ids.txt \
-      --missing --hardy midp --out hwe_testing/hardy_out/${chr}_${anc} --extract bed1 all_study_hits.bed ;\
+    ./plink2 --pfile pgen_qc/chr${chr}_geno_mac --keep ${anc}_ids.txt \
+      --hardy midp --out hwe_testing/hardy_out/${chr}_${anc} --extract bed1 hwe_testing/all_study_hits.bed ;\
   done \
 done
 
@@ -36,26 +47,27 @@ for anc in "${ancestries[@]}"; do \
   done \
 done
 
-# Rcode for producing a table for these results
-aou_hits = vroom("aou_ad_any_anc_all_gwas_geno_1e-1_mac20_common_pcs_pvals_gwsig.txt",show_col_types=F) %>% 
-    arrange(CHROM,GENPOS) 
-anc_hwe = data.frame(ID=aou_hits$ID,MIDP_EUR=NA,MIDP_AFR=NA,MIDP_AMR=NA,MIDP_EAS=NA,MIDP_SAS=NA,MIDP_MID=NA) 
-
-hwe_datasets = lapply(X = c("eur","afr","amr","eas","sas","mid"),FUN = function(x) {
-    vroom(glue("hwe_testing/hardy_out/{x}_hwe_stats.txt"),show_col_types = F) %>% 
-    distinct(ID,.keep_all = T)
+#############
+# RCode on AoU to merge output into one table
+all_var = vroom("hwe_testing/hardy_out/all_hwe_stats.txt",show_col_types = F) %>% 
+    separate(ID,sep = "-",into=c("CHR",'POS'),remove = F) %>%
+    mutate(IDrev = glue("{CHR}-{POS}-{AX}-{A1}")) %>% select(ID,IDrev,MIDP)
+anc_hwe = data.frame(ID=all_var$ID,IDrev=all_var$IDrev,MIDP_ALL=NA,MIDP_AFR=NA,MIDP_AMR=NA,
+                    MIDP_EAS=NA,MIDP_EUR=NA,MIDP_MID=NA,MIDP_SAS=NA)
+hwe_datasets = lapply(X = c('all','afr','amr','eas','eur','mid','sas'),FUN = function(x) {
+    vroom(glue("hwe_testing/hardy_out/{x}_hwe_stats.txt"),show_col_types = F)
 })
-
 for (row in 1:nrow(anc_hwe)) {
-    curr_id = anc_hwe$ID[[row]]    
-    for (anc in c(1:6)) {
-        curr_anc_id = which(hwe_datasets[[anc]]$ID == curr_id)
+    curr_id = anc_hwe$ID[[row]] ; curr_id_rev = anc_hwe$IDrev[[row]]
+    for (anc in c(1:7)) {
+        curr_anc_id = which(hwe_datasets[[anc]]$ID == curr_id | hwe_datasets[[anc]]$ID == curr_id_rev)
+        if (length(curr_anc_id) > 1) curr_anc_id = which(hwe_datasets[[anc]]$ID == curr_id)
         if (length(curr_anc_id) > 0)
-            anc_hwe[[row,(1+anc)]] = hwe_datasets[[anc]]$MIDP[[curr_anc_id]]
+            anc_hwe[[row,(2+anc)]] = hwe_datasets[[anc]]$MIDP[[curr_anc_id]]
     }
 }
 head(anc_hwe)
-vroom_write(anc_hwe,"anc_hwe_midp.txt")
+vroom_write(anc_hwe,"hwe_testing/anc_hwe_midp.txt")
 
 print(nrow(anc_hwe))
 print(nrow(anc_hwe %>% filter(MIDP_EUR <= 1e-15 | MIDP_AFR <= 1e-15 | MIDP_AMR <= 1e-15 |
@@ -63,6 +75,8 @@ print(nrow(anc_hwe %>% filter(MIDP_EUR <= 1e-15 | MIDP_AFR <= 1e-15 | MIDP_AMR <
 print(nrow(anc_hwe %>% filter(MIDP_EUR <= 1e-15 | MIDP_AFR <= 1e-15 | MIDP_AMR <= 1e-15)))
 
 ####################################
+##############
+# BACK TO FASRC
 # To then give this file columns with single-ancestry regenie p-values, use the following:
 ancestries=(all eur afr amr) ;\
 for anc in "${ancestries[@]}"; do \
