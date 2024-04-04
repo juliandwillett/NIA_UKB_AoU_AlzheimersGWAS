65d9d465b01649bc6fec56a8646e782216e8c18d
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-04-04 09:43:10 -0400

    Create F.IntersectByGWASSignal.sh

---
+++ 3.MetaAnalysis/F.IntersectByGWASSignal.sh
@@ -0,0 +1,10 @@
+# INTERSECT BY CHRPOS, INTERSECT BY ID IN R WHERE IT IS EASIER TO BE PRECISE
+awk 'NR==FNR{arr[$18]; next} $16 in arr' working/gwas_gwsig_for_intersect.txt \
+  /n/home09/jwillett/true_lab_storage/Data_Links/AoU_GWAS/CommonPCs_NonMCC_Geno1e-1_MAC20/aou_ad_any_anc_all_gwas_geno_1e-1_mac20_common_pcs_pvals_chrpos.txt > \
+  working/gwas_hits_aou_intersect.txt
+awk 'NR==FNR{arr[$18]; next} $15 in arr' working/gwas_gwsig_for_intersect.txt \
+  /n/home09/jwillett/true_lab_storage/Data_Links/UKB_GWAS_Data/all_variants_200k_complete_p_id_chrpos.regenie > \
+  working/gwas_hits_ukb_intersect.txt
+awk 'NR==FNR{arr[$18]; next} $15 in arr' working/gwas_gwsig_for_intersect.txt \
+  /n/home09/jwillett/true_lab_storage/Data_Links/NIAGADS_Personal/NIAGADS_meta_chrpos.txt > \
+  working/gwas_hits_niagads_intersect.txt
