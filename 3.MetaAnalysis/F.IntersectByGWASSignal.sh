86028ff24d4c6a70c44c6c799f0db5248869b4a6
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-04-16 14:49:40 -0400

    Update F.IntersectByGWASSignal.sh

--- 3.MetaAnalysis/F.IntersectByGWASSignal.sh
+++ 3.MetaAnalysis/F.IntersectByGWASSignal.sh
@@ -1,6 +1,8 @@
 # INTERSECT BY CHRPOS, INTERSECT BY ID IN R WHERE IT IS EASIER TO BE PRECISE
+awk 'NR==1 {print}' /n/home09/jwillett/true_lab_storage/00_AoU/aou_ukb_nia_allvar_meta_analysis_chrposrefalt_cols.TBL >\
+  working/gwas_hits_meta_intersect.txt ;\
 awk 'NR==FNR{arr[$1]; next} $1 in arr' working/gwas_gwsig_for_intersect_ids.txt \
-  /n/home09/jwillett/true_lab_storage/00_AoU/aou_ukb_nia_allvar_meta_analysis_chrposrefalt_cols.TBL > \
+  /n/home09/jwillett/true_lab_storage/00_AoU/aou_ukb_nia_allvar_meta_analysis_chrposrefalt_cols.TBL >> \
   working/gwas_hits_meta_intersect.txt ;\
 awk 'NR==FNR{arr[$3]; next} $16 in arr' working/gwas_gwsig_for_intersect.txt \
   /n/home09/jwillett/true_lab_storage/Data_Links/AoU_GWAS/CommonPCs_NonMCC_Geno1e-1_MAC20/aou_ad_any_anc_all_gwas_geno_1e-1_mac20_common_pcs_pvals_chrpos.txt > \
