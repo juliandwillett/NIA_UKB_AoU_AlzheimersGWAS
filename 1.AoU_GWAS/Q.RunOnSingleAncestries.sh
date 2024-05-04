562e7960c6ab898b675aa96a47fc2f76f03f94d7
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-05-04 16:19:46 -0400

    Update Q.RunOnSingleAncestries.sh

--- 1.AoU_GWAS/Q.RunOnSingleAncestries.sh
+++ 1.AoU_GWAS/Q.RunOnSingleAncestries.sh
@@ -11,3 +11,33 @@ for anc in "${ancestries[@]}"; do \
   ./plink2 --pfile array_data/arrays_${anc}_autosomes_post_qc_pruned --pca 20 approx \
     --out array_data/arrays_${anc}_autosomes_post_qc_pruned_pcs ;\
 done
+
+# R WORK
+covar = vroom("regenie_input/regenie_covar.txt",show_col_types = F) %>% select(FID,IID,Age,Sex)
+for (anc in c("eur", "amr", "afr")) {
+    pcs = vroom(glue("array_data/arrays_{anc}_autosomes_post_qc_pruned_pcs.eigenvec")) %>% rename(IID = `#IID`)
+    anc_covar = merge(covar,pcs,by='IID') %>% relocate(FID,.before='IID')
+    vroom_write(anc_covar,glue("regenie_input/regenie_covar_{anc}.txt"))
+}
+
+# REGENIE STEP 1
+ancestries=(eur amr afr) ;\
+for anc in "${ancestries[@]}"; do \
+  awk 'NR==1 {print "#FID\tIID\tSEX"} NR>1 {print "0\t" $1 "\t" "NA"}' array_data/arrays_${anc}_autosomes_post_qc_pruned.psam > tmp ;\
+  mv tmp array_data/arrays_${anc}_autosomes_post_qc_pruned.psam ;\
+done
+
+for anc in "${ancestries[@]}"; do \
+    ./regenie_v3.4.1.gz_x86_64_Centos7_mkl \
+    --step 1 \
+    --pgen array_data/arrays_${anc}_autosomes_post_qc_pruned \
+    --phenoFile regenie_input/regenie_pheno.txt \
+    --covarFile regenie_input/regenie_covar_${anc}.txt \
+    --bt \
+    --out rg_step1_singleanc/aou_step1_rg_common_${anc} \
+    --bsize 1000 \
+    --lowmem \
+    --lowmem-prefix tmp_rg_20 \
+    --phenoCol AD_any ;\
+done ;\
+gsutil -m cp -rn rg_step1_singleanc/aou_step1_rg_common* $WORKSPACE_BUCKET/data/regenie_step1_singleanc_anc_pcs/
