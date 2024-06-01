4da190b0de5254d44e7af8bb1ef2403d9bf8b1dc
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-06-01 16:42:59 -0400

    Update S.RandomCaseControl.sh

--- 1.AoU_GWAS/S.RandomCaseControl.sh
+++ 1.AoU_GWAS/S.RandomCaseControl.sh
@@ -20,3 +20,21 @@ vroom_write(tmp,'regenie_input/regenie_pheno_randomcases_5percent.txt')
     --lowmem \
     --lowmem-prefix tmp_rg_40 \
     --phenoCol AD_any
+
+# Step 2
+for ((chr=1;chr<=22;chr++)); do \
+                ./regenie_v3.4.1.gz_x86_64_Centos7_mkl \
+                    --step 2 \
+                    --pgen pgen_qc/chr${chr}_geno_mac \
+                    --phenoFile regenie_input/regenie_pheno_randomcases_5percent.txt \
+                    --covarFile regenie_input/regenie_covar_commonpcs.txt \
+                    --bt --firth-se \
+                    --firth --approx --pThresh 0.01 \
+                    --pred step1_randomcase/aou_step1_randomcase_pred.list \
+                    --bsize 400 \
+                    --out step2_randomcase/chr${chr} \
+                    --minMAC 20 \
+                    --phenoCol AD_any ;\
+done
+
+# Back up
