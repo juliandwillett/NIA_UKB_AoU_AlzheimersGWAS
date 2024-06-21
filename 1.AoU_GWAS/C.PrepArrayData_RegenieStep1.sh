62cce6b6dfcc2385d2fadaee277ede2d397f01aa
juliandwillett <81776781+juliandwillett@users.noreply.github.com>
2024-06-21 11:12:37 -0400

    Update C.PrepArrayData_RegenieStep1.sh

--- 1.AoU_GWAS/C.PrepArrayData_RegenieStep1.sh
+++ 1.AoU_GWAS/C.PrepArrayData_RegenieStep1.sh
@@ -5,8 +5,7 @@ gsutil -u $GOOGLE_PROJECT -m cp -r $WORKSPACE_BUCKET/data/array_data_pgen_files/
 # Do QC
 ./plink2 \
   --pfile arrays_allchr \
-  --maf 0.01 --mac 100 --geno 0.01 --hwe 1e-15 \
-  --mind 0.05 \ 
+  --maf 0.01 --mac 100 --geno 0.1 \
   --make-pgen --chr 1-22 \
   --out arrays_autosomes_post_qc \
   --indep-pairwise 100kb 1 0.1
